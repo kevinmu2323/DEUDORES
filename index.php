3593db03ffd88cdd7a87d40d37114cfdcd91cb2c
Kevin <Kevinmurias23@gmail.com>
2025-05-04 13:32:29 -0300

    prueba3

--- index.php
+++ index.php
@@ -1,131 +1,103 @@
 <?php
-include 'clases.php';
-$ruta= congif::ruta();
-$buscar=$_POST['buscar']??"";
-$buscar=strtoupper($buscar);
-$resultado="";
-$total=0;
-$eliminar=$_POST['eliminar']??"";
-$eliminar=strtoupper($eliminar);
-$lineanueva=[];
-
-$archivo=fopen($ruta,"r");
-while(($lineas=fgetcsv($archivo,0,"|")??"")!== false)
-{  
-
-    if($lineas[1]===$buscar)
-    {
-       $resultado.="<tr> <td>{$lineas[0]}</td>
-                <td>{$lineas[1]}</td>
-                <td> $ {$lineas[2]}</td>
-                <td>{$lineas[3]}</td>
-                </tr>"; 
-                $total+= (float) $lineas[2];
-    }
-    
-    
-}
-fclose($archivo);
-$archivo=fopen($ruta,"r");
-while(($lineas=fgetcsv($archivo,0,"|")??"")!== false)
-{  
-
-    if($lineas[1]!=$eliminar)
-    {
-       $lineanueva[]=$lineas;
-    }
-    
-    
-}
-fclose($archivo);
-
-$archivo=fopen($ruta,"w");
-foreach($lineanueva as $lineas)
+if($_SERVER['REQUEST_METHOD']==='POST')
 {
-    fputcsv($archivo,$lineas,"|");
+    include 'clases.php';
+    include 'consulta.php';
+$name=$_POST['name']??"";
+$valor=$_POST['valor'];
+$mensaje=$_POST['mensaje'];
+$ruta= congif :: ruta();
+$name=strtoupper($name);
+$result="";
+$id=1;
+$archivo=fopen($ruta,"r");
+while(!feof($archivo))
+{
+    $lineas=fgetcsv($archivo,0,"|");
+    if($lineas[0]=="")
+    {
+        continue;
+    }
+    else
+    {
+        $id=$lineas[0]+1;
+    }
 }
 fclose($archivo);
+$datos="$id|$name|$valor|$mensaje|\n";
+$guardar=new Guardar();
+$resultado=$guardar->guardar1($datos,$ruta);
 
+
+
+}
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Consultas</title>
-    <style>
-  nav {  
+<!DOCTYPE html>  
+<html lang="es">  
+<head>  
+    <meta charset="UTF-8">  
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+    <title>Deudores</title>  
+    <style>  
+        body {  
+            font-family: Arial, sans-serif;  
+            margin: 0;  
+            padding: 0;  
+            box-sizing: border-box;  
+            background-color:rgb(134, 134, 134);
+        }  
+        header {  
+            background-color: #4CAF50;  
+            color: white;  
+            padding: 10px 20px;  
+            text-align: center;  
+        }  
+        nav {  
             margin: 10px 0;  
         }  
         nav a {  
             margin: 0 15px;  
             color: white;  
             text-decoration: none;  
-        }          
-body
-{  
-        font-family: Arial, sans-serif;  
-        margin: 0;  
-        padding: 0;  
-        box-sizing: border-box;  
-        background-color:rgb(134, 134, 134);
-}  
-header 
-{  
-    background-color: #4CAF50;  
-    color: white;  
-    padding: 10px 20px;  
-    text-align: center;  
-} 
-.container2
-{
+        }  
+        .container, .container2 {
     width: 90%;
     max-width: 400px;
     margin: auto;
     padding: 3%;
     background-color: rgba(206, 206, 206, 0.53);
-    justify-content: center;
-}
-table
-{
-    background-color: rgb(245, 245, 245);
-    color:rgba(0, 0, 0, 0.94);
 }
 
-    </style>
-</head>
-<body>
-    <header>  
-    <nav>  
-        <a href="index.php">Inicio</a>  
-        <a href="consulta.php">consultas</a>  
-    </nav>  
-    </header>  
-            <div class="container2">
-            <table border="2" >
-                <form action="consulta.php" method="post">
-                <input type="text" id="buscar" name="buscar" placeholder="buscar"> <input type="submit">
-                </form>
-            <tr><td>id</td><td>Nombre</td><td>Deuda</td><td>informacion</td></tr>
-        <?php   
-        $archivo=fopen($ruta,"r");
-        while(($lineas=fgetcsv($archivo,0,"|")??"")!== false)
-        {  
-            echo"<tr> <td>{$lineas[0]}</td>
-                <td>{$lineas[1]}</td>
-                <td> $ {$lineas[2]}</td>
-                <td>{$lineas[3]}</td>
-                </tr>";   
+        
+        p
+        {
+            margin-left:-4%;
+            background-color:#FFF; 
         }
-?>
-<form action="consulta.php" method="post">
-                <input type="text" id="eliminar" name="eliminar" placeholder="ELIMINAR"> <input type="submit">
-                </form>
-        </table>
-        <table><tr><?php  echo $resultado;?></tr></table>
-        <p><?php  echo $total;?></p>
-        </div>
-     
-</body>
-</html>
+    </style>  
+</head>  
+<body>  
+
+<header>  
+    <nav>  
+        <a href="index.html">Inicio</a>  
+        <a href="consulta.php">consultas</a>  
+        
+    </nav>  
+</header>  
+
+<div class="container">  
+<form  action="archivo.php" method="post"> <br>
+<p>Nombre</p>
+<input type="text" id="name" name="name"><br>
+<p>Deuda</p>
+<input type="number" id="valor" name="valor"><br>
+<p>Informacion</p>
+<input type="text" id="mensaje" name="mensaje"><br>
+<input type="submit">
+
+<tr> <td><?php echo $resultado; ?></td></tr>
+</div >  
+</body>  
+</html>  
